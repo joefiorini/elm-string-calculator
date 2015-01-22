22c2dab88d819eb065781cd7921d7ce66bee68aa
Joe Fiorini <joe@joefiorini.com>
2015-01-22 08:32:54 -0500

    Layout html elements

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,48 @@
-import Html (p, text)
+import Html (main', section, input, label, text, select, option, p)
+import Html.Attributes (type')
 import Text (plainText)
 
 import Doubler
 
-main =
-  p
+delimiterInput =
+  section
     []
-    [text (Doubler.double 2 |> toString)]
+    [ label
+        []
+        [text "Choose Delimiter"]
+    , select
+        []
+        [ option [] [text ","]
+        , option [] [text "|"]
+        , option [] [text "+"]
+        , option [] [text "/"]
+        ]
+    ]
+
+numberInput =
+  section
+    []
+    [ label
+        []
+        [ text "Enter some numbers seperated by chosen delimiter:"
+        ]
+    , input
+        [ type' "text" ]
+        []
+    ]
+
+numberOutput result =
+  section
+    []
+    [ p
+      []
+      [ text <| "Your result is: " ++ result]
+    ]
+
+main =
+  main'
+    []
+    [ delimiterInput
+    , numberInput
+    , numberOutput ""
+    ]
